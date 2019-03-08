1e90c679d3cbcadf0d38bcde3acf11db24d51617
Sebastián Gaviria <sgaviria@colegiobolivar.edu.co>
2019-03-08 13:45:25 -0500

    ProductThumb with styles

--- src/components/ProductThumb/ProductThumb.tsx
+++ src/components/ProductThumb/ProductThumb.tsx
@@ -14,8 +14,16 @@ interface ProductThumbProps {
 const ProductThumb = function({ name, price, image, id }: ProductThumbProps) {
   return (
     <div className="ProductThumb">
-      <img src={image} />
-      <h2>{name}</h2>
+      <div className="ProductThumb__front">
+        <h2 className="ProductThumb__title">{name}</h2>
+        <img className="ProductThumb__image" src={image} />
+        <p className="ProductThumb__price">From £{price}</p>
+      </div>
+      <div className="ProductThumb__back">
+        <h2 className="ProductThumb__title">{name}</h2>
+        <p className="ProductThumb__price">From £{price}</p>
+        <Link className="Button" to="/store/gadasd">Quick view</Link>
+      </div>
     </div>
   );
 }
